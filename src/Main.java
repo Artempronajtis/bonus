735b5a08ef710ed960dcb869dd94c37bce25607b
Artem test <artempronajtis@gmail.com>
2024-01-29 20:02:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 20;
+        int ticketPrice = 3000;
+        int miles = ticketPrice / bonus;
+
+        System.out.println("Итоговый бонус: " + miles);
+
+
+    }
+}
+
+
